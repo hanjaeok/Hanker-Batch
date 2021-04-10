423e402f705c45ad4ed99a00d624244b1a953d9b
han-ker <hanjaeok17@gmail.com>
2021-04-11 01:15:36 +0900

    1. pom.xml spring-boot-starter-quartz 추가

---
+++ src/main/java/com/hanker/hankerbatch/HankerBatchApplication.java
@@ -0,0 +1,13 @@
+package com.hanker.hankerbatch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HankerBatchApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HankerBatchApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hanker/hankerbatch/HankerBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanker.hankerbatch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HankerBatchApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
